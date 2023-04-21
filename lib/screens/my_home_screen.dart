b7a75af1e277d8f4f4bb8cb4a48f943c7efc2024
safwan4286 <safwanbolwala4286@gmail.com>
2023-04-21 15:12:50 +0530

    test4

--- lib/screens/my_home_screen.dart
+++ lib/screens/my_home_screen.dart
@@ -62,7 +62,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
                             Navigator.pop(context);
                           },
-                          child: const Text('Submit'),
+                          child: const Text('Submit '),
                         )
                       ],
                     ),
